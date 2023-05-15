4b9db78a1d219bed417a3d8cf9e08d008aa46561
khanh-devos <dreamproperty.khanh@gmail.com>
2023-05-15 21:57:58 +0700

    show and hide page Menu with "hambergur button"
    - link index.html to menu.js
    - setup page Menu with fixed position floating over the body
    - link anchor in page Menu to the proper section by id

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function showMenu(x){
+  const mobile_menu = document.querySelector('#mobile-menu');
+  mobile_menu.style.display = "block";
+}
+
+function hideMenu(x){
+  const mobile_menu = document.querySelector('#mobile-menu');
+  mobile_menu.style.display = "none";  
+}
