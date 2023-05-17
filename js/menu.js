f14efb0fa38f523954dfea9395fab2e11ea1fa33
khanh-devos <dreamproperty.khanh@gmail.com>
2023-05-17 12:33:16 +0700

    remove hideMenu and showMenu, add popup window
    - improve toggleHamburgerBtn() to use it for all anchors <a> in MenuPage
    - add popup window.

--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,17 @@
-function showMenu() {
-  const mobileMenu = document.querySelector('#mobile-menu');
-  const img = document.querySelector('#menu-btn img');
-  const a = document.querySelector('#header a');
-
-  mobileMenu.style.display = 'block';
-  img.src = './imgs/x.svg';
-  a.style.opacity = 0;
-}
-
-function hideMenu() {
-  const mobileMenu = document.querySelector('#mobile-menu');
-  const img = document.querySelector('#menu-btn img');
-  const a = document.querySelector('#header a');
-
-  mobileMenu.style.display = 'none';
-  img.src = './imgs/IconMenu.svg';
-  a.style.opacity = 1;
-}
-
 function toggleHamburgerBtn() {
   const mobileMenu = document.querySelector('#mobile-menu');
+  const img = document.querySelector('#menu-btn img');
+  const logo = document.querySelector('#header a');
+
+  // expression
   if (mobileMenu.style.display === 'none' || !mobileMenu.style.display) {
-    showMenu();
+    mobileMenu.style.display = 'block';
+    img.src = './imgs/x.svg';
+    logo.style.opacity = 0;
   } else {
-    hideMenu();
+    mobileMenu.style.display = 'none';
+    img.src = './imgs/IconMenu.svg';
+    logo.style.opacity = 1;
   }
 }
 
